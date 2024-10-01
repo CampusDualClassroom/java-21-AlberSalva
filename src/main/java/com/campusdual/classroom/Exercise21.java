0a2d2b0c69dd5f9634d44a3619c624f1509cf972
Alberto Salvado Fernández <95777814+AlberSalva@users.noreply.github.com>
2024-10-01 11:29:21 +0200

    Implemented createHashSet(), createTreeSet(), addElementToSet() and printSet()

--- src/main/java/com/campusdual/classroom/Exercise21.java
+++ src/main/java/com/campusdual/classroom/Exercise21.java
@@ -1,5 +1,7 @@
 package com.campusdual.classroom;
 
+import com.sun.source.tree.Tree;
+
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
@@ -7,24 +9,51 @@ import java.util.TreeSet;
 public class Exercise21 {
 
     public static Set<String> createHashSet() {
+        HashSet<String> hashSet = new HashSet<String>();
+        hashSet.add("ELEMENT KPRBC");
+        hashSet.add("ELEMENT YPBTM");
+        hashSet.add("ELEMENT AADXU");
+        hashSet.add("ELEMENT RXCGJ");
+        hashSet.add("ELEMENT WYMVD");
+        hashSet.add("ELEMENT WFGEJ");
+        hashSet.add("ELEMENT TYGBS");
+        hashSet.add("ELEMENT MAPTK");
+        hashSet.add("ELEMENT GJXVE");
+        hashSet.add("ELEMENT BAFGL");
 
+        return hashSet;
     }
 
     public static Set<String> createTreeSet() {
+        TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.add("ELEMENT KPRBC");
+        treeSet.add("ELEMENT YPBTM");
+        treeSet.add("ELEMENT AADXU");
+        treeSet.add("ELEMENT RXCGJ");
+        treeSet.add("ELEMENT WYMVD");
+        treeSet.add("ELEMENT WFGEJ");
+        treeSet.add("ELEMENT TYGBS");
+        treeSet.add("ELEMENT MAPTK");
+        treeSet.add("ELEMENT GJXVE");
+        treeSet.add("ELEMENT BAFGL");
 
+        return treeSet;
     }
 
     private static void printSet(Set<String> customSet) {
-
+        System.out.println(customSet);
     }
 
 
     public static boolean addElementToSet(Set<String> set, String elementToAdd) {
-
+        return set.add(elementToAdd);
     }
 
     public static void main(String[] args) {
-
+        Set<String> myHashSet = createHashSet();
+        Set<String> myTreeSet = createTreeSet();
+        printSet(myHashSet);
+        printSet(myTreeSet);
     }
 
 }
